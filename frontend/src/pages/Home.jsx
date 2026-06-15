b810a6ba4a5bc06ab42080715890397234a9f659
james-nightlife <sutthiphong.kan@outlook.com>
2026-06-15 15:52:51 +0700

    feat: enhance Home component with improved data handling, pagination, and user information display

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,17 +8,17 @@ import { saveAs } from 'file-saver';
 const Home = () => {
   const navigate = useNavigate();
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   const fetchData = async () => {
     try{
       const res = await axios.get(
         `${import.meta.env.VITE_API}/job-record`,
         {
-            headers: {
-              'Authorization': localStorage.getItem('token'),
-              "Content-Type": false,
-            }
+          headers: {
+            'Authorization': localStorage.getItem('token'),
+            "Content-Type": false,
+          }
         }
       )
       var res_data = res.data.sort((x, y) => (new Date(y.workDate) - new Date(x.workDate) || x.name.localeCompare(y.name) ))
@@ -29,32 +29,38 @@ const Home = () => {
         alert('เซสชันหมดอายุ กรุณาลงชื่อเข้าใช้งานระบบใหม่')
         return navigate('/sign-in');
       }
-      alert('เกิดปัญหาทางเทคนิค')
+        alert('เกิดปัญหาทางเทคนิค')
     }
   }
 
   useEffect(() => {
     if(!localStorage.getItem('token')) {
-      return navigate('/sign-in');
+      navigate('/sign-in');
+      return;
     }
     fetchData();
   }, [])
+
+  const mytask = data.filter(x => x.username === localStorage.getItem('username'));
+  const mytask_minutes = mytask.reduce((acc, x) => acc + (x.hours * 60) + x.minutes, 0);
 
   const [search, setSearch] = useState("");
 
   /** PAGINATION */
 
   const filteredData = useMemo(() => {
-        return data.filter((item) => {
-        const searchStr = search.toLowerCase();
-        return (
-            item.name?.toLowerCase().includes(searchStr) ||
-            item.username?.toLowerCase().includes(searchStr) ||
-            item.description?.toLowerCase().includes(searchStr) ||
-            item.progression?.toLowerCase().includes(searchStr)
-        )
-        });
-    }, [data, search]);
+    return data.filter((item) => {
+      const searchStr = search.toLowerCase();
+      return (
+        item.name?.toLowerCase().includes(searchStr) ||
+        item.username?.toLowerCase().includes(searchStr) ||
+        item.description?.toLowerCase().includes(searchStr) ||
+        item.progression?.toLowerCase().includes(searchStr)
+      )
+    });
+  }, [data, search]);
+
+  const data_minutes = filteredData.reduce((acc, x) => acc + (x.hours * 60) + x.minutes, 0);
 
   const [page, setPage] = useState(1);
   const offset = 10;
@@ -62,17 +68,17 @@ const Home = () => {
   //const dataPreview = data.slice((page-1)*(offset), ((page-1)*offset)+(offset))
 
   const dataPreview = useMemo(() => {
-        const start = (page - 1) * offset;
-        return filteredData.slice(start, start + offset);
+    const start = (page - 1) * offset;
+    return filteredData.slice(start, start + offset);
   }, [filteredData, page, offset]);
 
   useEffect(() => {
-        setPage(1);
+    setPage(1);
   }, [search]);
 
   useEffect(() => {
-      setPage(1)
-      //console.log('data', data)
+    setPage(1)
+    //console.log('data', data)
   }, [data])
 
   /** SIGN OUT */
@@ -105,140 +111,154 @@ const Home = () => {
   }
 
   const exportToExcel = async (e, data) => {
-        const worksheet = XLSX.utils.json_to_sheet(data);
-        const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
-        // Buffer the output and trigger download
-        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-        const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
-        saveAs(blob, `job_recorder_${new Date().getTime()}.xlsx`);
+    // Buffer the output and trigger download
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    saveAs(blob, `job_recorder_${new Date().getTime()}.xlsx`);
   };
 
   return (
     <>
-    <div className='min-h-screen bg-gray-50 p-2 sm:p-4 md:p-8'>
-      <div className='max-w-7xl mx-auto flex flex-col gap-6 bg-white border shadow-sm p-4 sm:p-6 rounded-lg'>
-        <div className='flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4'>
-          <h1 className='text-center'>ระบบรายงานการปฏิบัติงานประจำวัน</h1>
-          <Link 
-            to="/new-record"
-            className='border text-center bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded'>
-            สร้างบันทึก
-          </Link>
-
-        </div>
-        <div className="flex gap-2 items-center">
+      <div className='min-h-screen bg-gray-50 p-2 sm:p-4 md:p-8'>
+        <div className='max-w-7xl mx-auto flex flex-col gap-6 bg-white border shadow-sm p-4 sm:p-6 rounded-lg'>
+          <div className='flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4'>
+            <h1 className='text-center'>ระบบรายงานการปฏิบัติงานประจำวัน</h1>
+            <Link 
+              to="/new-record"
+              className='border text-center bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded'>
+              สร้างบันทึก
+            </Link>
+          </div>
+          {/** ค้นหาข้อมูล */}
+          <div className="flex gap-2 items-center">
             <label className="font-bold">ค้นหาข้อมูล</label>
             <input
-            type="text"
-            placeholder="ค้นหาบัวศรีไอดี ชื่อบุคลากร รายละเอียดงาน หรือรายละเอียดความคืบหน้าหรือความสำเร็จ"
-            className="border p-2 rounded-lg w-full md:w-1/2"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            />
-            <div>ผลลัพธ์ {filteredData.length} จากทั้งหมด {data.length} รายการ</div>
-        </div>
-        
-        <div className='w-full overflow-x-auto'>
-           {dataPreview.length ? (
-        <table className='w-full border-collapse'>
-          <thead>
-            <tr>
-              <th className='whitespace-nowrap'>ที่</th>
-              <th className='whitespace-nowrap'>บุคลากร</th>
-              <th className='whitespace-nowrap'>วันที่ปฏิบัติงาน</th>
-              <th className='whitespace-nowrap'>รายละเอียดงาน</th>
-              <th className='whitespace-nowrap'>รายละเอียดความคืบหน้า / ความสำเร็จ</th>
-              <th>ระยะเวลา (ชั่วโมง)</th>
-              <th className='whitespace-nowrap'>เอกสารประกอบ</th>
-              <th className='whitespace-nowrap right-0 bg-gray-200'>จัดการ</th>
-            </tr>
-          </thead>
-          <tbody className='divide-y divide-gray-200'>
+              type="text"
+              placeholder="ค้นหาบัวศรีไอดี ชื่อบุคลากร รายละเอียดงาน หรือรายละเอียดความคืบหน้าหรือความสำเร็จ"
+              className="border p-2 rounded-lg w-full md:w-1/2"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)} />
+            <div>ผลลัพธ์ {filteredData.length} จากทั้งหมด {data.length} รายการ ({Math.floor(data_minutes / 420)} วัน {Math.floor((data_minutes % 420) / 60)} ชั่วโมง {data_minutes % 60} นาที)</div>
+          </div>
+          {/** ตารางแสดงข้อมูล */}
+          <div className='w-full overflow-x-auto'>
             {
-          dataPreview.map((x, idx) => {
-            return(
-              <tr key={idx} className='hover:bg-gray-50 transition-colors'>
-                <td className='text-center'>{(offset * (page - 1)) + (idx + 1)}</td>
-                <td className='whitespace-nowrap font-medium'>{x.name}</td>
-                <td className='whitespace-nowrap'>{new Date(x.workDate).toLocaleDateString('th-TH', {day: 'numeric', month: 'long', year: 'numeric'})}</td>
-                <td className='truncate max-w-62.5'>
-                    {x.description}     
-                </td>
-                <td className='truncate max-w-62.5'>
-                    {x.progression}    
-                </td>
-                <td className='text-center'>
-                  {`${x.hours}:${String(x.minutes).padStart(2,0)}`}
-                </td>
-                <td className='whitespace-nowrap'>{x.fileNames.map((x, idx) => 
-                  (
-                    <Link
-                      to={`${import.meta.env.VITE_API}/upload/${x}`}
-                      target='_blank'
-                      className='block'
-                      key={idx}>
-                        {x}
-                    </Link>
-                  )
-                )}</td>
-                <td className='right-0 bg-white md:bg-transparent'>
-                  { x.username === localStorage.getItem('username') &&
-                  <div className='flex flex-col'>
-                    <Link 
-                      to={`/edit/${x._id}`}
-                      className='border p-1 bg-blue-500 hover:bg-blue-400 text-white text-center'>แก้ไข</Link>
-                    <button 
-                      onClick={() => handleDelete(x._id)}
-                      className='border p-1 bg-red-500 hover:bg-red-400 text-white text-center'>
-                        ลบ
-                      </button>
-                  </div>
+              dataPreview.length ? (
+                <table className='w-full border-collapse'>
+                  <thead>
+                    <tr>
+                      <th className='whitespace-nowrap'>ที่</th>
+                      <th className='whitespace-nowrap'>บุคลากร</th>
+                      <th className='whitespace-nowrap'>วันที่ปฏิบัติงาน</th>
+                      <th className='whitespace-nowrap'>รายละเอียดงาน</th>
+                      <th className='whitespace-nowrap'>รายละเอียดความคืบหน้า / ความสำเร็จ</th>
+                      <th>ระยะเวลา (ชั่วโมง)</th>
+                      <th className='whitespace-nowrap'>เอกสารประกอบ</th>
+                      <th className='whitespace-nowrap right-0 bg-gray-200'>จัดการ</th>
+                    </tr>
+                  </thead>
+                  <tbody className='divide-y divide-gray-200'>
+                  {
+                    dataPreview.map((x, idx) => {
+                      return(
+                        <tr key={idx} className='hover:bg-gray-50 transition-colors'>
+                          <td className='text-center'>{(offset * (page - 1)) + (idx + 1)}</td>
+                          <td className='whitespace-nowrap font-medium'>{x.name}</td>
+                          <td className='whitespace-nowrap'>{new Date(x.workDate).toLocaleDateString('th-TH', {day: 'numeric', month: 'long', year: 'numeric'})}</td>
+                          <td className='truncate max-w-62.5'>
+                            {x.description}     
+                          </td>
+                          <td className='truncate max-w-62.5'>
+                            {x.progression}    
+                          </td>
+                          <td className='text-center'>
+                            {`${x.hours}:${String(x.minutes).padStart(2,0)}`}
+                          </td>
+                          <td className='whitespace-nowrap'>
+                            {
+                              x.fileNames.map((x, idx) => (
+                                <Link
+                                  to={`${import.meta.env.VITE_API}/upload/${x}`}
+                                  target='_blank'
+                                  className='block'
+                                  key={idx}>
+                                  {x}
+                                </Link>
+                              )
+                            )}
+                          </td>
+                          <td className='right-0 bg-white md:bg-transparent'>
+                          { 
+                            x.username === localStorage.getItem('username') &&
+                            <div className='flex flex-col'>
+                              <Link 
+                                to={`/edit/${x._id}`}
+                                className='border p-1 bg-blue-500 hover:bg-blue-400 text-white text-center'>แก้ไข</Link>
+                              <button 
+                                onClick={() => handleDelete(x._id)}
+                                className='border p-1 bg-red-500 hover:bg-red-400 text-white text-center'>
+                                ลบ
+                              </button>
+                            </div>
+                          }
+                          </td>
+                        </tr>
+                      )
+                    })
                   }
-                </td>
-              </tr>
-            )
-          })
-        }
-          </tbody>
-        </table>)
-        : 'ไม่พบข้อมูล'}
-</div>
-<div className='flex gap-4 items-center'>
-                <button onClick={() => page > 1 && setPage(page-1)} className='flex-1 cursor-pointer'>ก่อนหน้า</button>
-                <div>หน้า </div>
-                  <input 
-                  value={page} 
-                  name='page' 
-                  type='number' 
-                  min={1} 
-                  max={pages} 
-                  onChange={(e) => {
-                    const val = parseInt(e.target.value, 10);
-                    if (!isNaN(val) && (val < pages+1) && (val > 0)) {
-                      setPage(val);
-                    }}} />
-                    <div> จาก {pages} </div>
-                <button onClick={() => page < pages && setPage(page+1)} className='flex-1 cursor-pointer'>ถัดไป</button>
-            </div>
-<div className='flex justify-end mt-4 gap-4'>
-  {
-    localStorage.getItem('username') === 'songyot' &&
-    <>
-      <button className='bg-green-500' onClick={(e) => exportToExcel(e, data)}>
-                    ดาวน์โหลดเป็น Excel
-      </button>
-    </>
-  }
-  <button 
-  onClick={handleSignOut}
-  className='bg-red-500 hover:bg-red-400'>
-      ออกจากระบบ
-    </button>
-</div>
-      </div>
-    </div> 
+                  </tbody>
+                </table>) : 
+                'ไม่พบข้อมูล'
+              }
+          </div>
+          {/** หน้า */}
+          <div className='flex gap-4 items-center'>
+            <button onClick={() => page > 1 && setPage(page-1)} className='flex-1 cursor-pointer'>ก่อนหน้า</button>
+            <div>หน้า </div>
+            <input 
+              value={page} 
+              name='page' 
+              type='number' 
+              min={1} 
+              max={pages} 
+              onChange={(e) => {
+                const val = parseInt(e.target.value, 10);
+                if (!isNaN(val) && (val < pages+1) && (val > 0)) {
+                  setPage(val);
+                }}} />
+            <div> จาก {pages} </div>
+            <button onClick={() => page < pages && setPage(page+1)} className='flex-1 cursor-pointer'>ถัดไป</button>
+          </div>
+          {/** ข้อมูลส่วนบุคคล */}
+          <div className='border p-4 rounded'>
+            <h1 className='text-center'>ข้อมูลส่วนบุคคล</h1>
+            <div>ชื่อผู้ใช้งาน: {localStorage.getItem('username')}</div>
+            <div>ชื่อบุคลากร: {localStorage.getItem('name')}</div>
+            <div>จำนวนบันทึก: {mytask.length} รายการ</div>
+            <div>รวมเวลาทำงาน: {Math.floor(mytask_minutes / 420)} วัน {Math.floor((mytask_minutes % 420) / 60)} ชั่วโมง {mytask_minutes % 60} นาที</div>
+          </div>
+          {/** เมนู */}
+          <div className='flex justify-end mt-4 gap-4'>
+            {
+              localStorage.getItem('username') === 'songyot' &&
+              <>
+                <button className='bg-green-500' onClick={(e) => exportToExcel(e, data)}>
+                  ดาวน์โหลดเป็น Excel
+                </button>
+              </>
+            }
+            <button 
+              onClick={handleSignOut}
+              className='bg-red-500 hover:bg-red-400'>
+              ออกจากระบบ
+            </button>
+          </div>
+        </div>
+      </div> 
     </>
   )
 }
